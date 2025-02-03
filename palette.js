dfdf5ba395244b9357f17539088f6a1de3a423fc
Mukul Singhal <work.mukulsinghal@gmail.com>
2025-02-03 22:51:27 +0530

    final js file

--- palette.js
+++ palette.js
@@ -1,19 +1,43 @@
 document.getElementById("gen").addEventListener("click", function() {
-  
-function RndColor() {
- const hex = "123456789ABCDEF";
- let color = "#";
 
+  function RndColor() {
+    const hex = "0123456789ABCDEF";
+    let color = "#";
 
-
-  for (let i =0; i <6; i++) {
-      color += hex[Math.floor(Math.random()*16)];
+    for (let i = 0; i < 6; i++) {
+      color += hex[Math.floor(Math.random() * 16)];
     }
     return color;
   }
 
   const boxes = document.querySelectorAll(".box");
+
   boxes.forEach(function(box) {
-    box.style.backgroundColor = RndColor();
+    const color = RndColor();
+    box.style.backgroundColor = color;
+    const span = box.querySelector("span");
+    span.textContent = color;
+  });
+
+  document.querySelectorAll(".copy-icon").forEach(function(copyIcon) {
+    copyIcon.removeEventListener('click', handleClick); // Clean up previous listeners
+    copyIcon.addEventListener('click', function() { handleClick(copyIcon) }); // Add listener
   });
 });
+
+function handleClick(copyIcon) {
+  const code = copyIcon.parentElement.querySelector("span").textContent;
+
+  navigator.clipboard.writeText(code).then(function() {
+    const popup = document.createElement("div");
+    popup.classList.add("popup");
+    popup.textContent = "Copied!";
+    copyIcon.parentElement.appendChild(popup);
+
+    setTimeout(function() {
+      popup.remove();
+    }, 1000);
+  }).catch(function(err) {
+    console.error("failed to copy: ", err);
+  });
+}
